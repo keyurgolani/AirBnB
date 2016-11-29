95659e483ed42d52e55874d29c42f8111916b9e4
vedvasa <vedvasa@gmail.com>
2016-11-28 23:06:39 -0800

    added Ui for multiple images upload

--- public/angularjs/anguApp.js
+++ public/angularjs/anguApp.js
@@ -1,4 +1,4 @@
-var airBnB = angular.module('airBnB', [ 'ngAnimate', 'focus-if', 'ngAutocomplete', 'ngMessages', 'ngRangeSlider', 'ngMap', 'nvd3' ])
+var airBnB = angular.module('airBnB', [ 'ngAnimate', 'focus-if', 'ngAutocomplete', 'ngMessages', 'ngRangeSlider', 'ngMap', 'nvd3', 'angularFileUpload' ])
 	.config([ '$locationProvider', function($locationProvider) {
 		$locationProvider.html5Mode({
 			enabled : true,
@@ -685,4 +685,153 @@ var airBnB = angular.module('airBnB', [ 'ngAnimate', 'focus-if', 'ngAutocomplete
 			var year = date.getFullYear();
 			return year + '-' + (month + 1) + '-' + day;
 		}
-	});
+	})
+	// .controller('fileUploadController', [ '$scope', 'FileUploader', function($scope, FileUploader) {
+ //        var uploader = $scope.uploader = new FileUploader({
+ //            url: 'property.ejs'
+ //        });
+
+ //        // FILTERS
+
+ //        uploader.filters.push({
+ //            name: 'customFilter',
+ //            fn: function(item /*{File|FileLikeObject}*/, options) {
+ //                return this.queue.length < 10;
+ //            }
+ //        });
+
+ //        // CALLBACKS
+
+ //        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+ //            console.info('onWhenAddingFileFailed', item, filter, options);
+ //        };
+ //        uploader.onAfterAddingFile = function(fileItem) {
+ //            console.info('onAfterAddingFile', fileItem);
+ //        };
+ //        uploader.onAfterAddingAll = function(addedFileItems) {
+ //            console.info('onAfterAddingAll', addedFileItems);
+ //        };
+ //        uploader.onBeforeUploadItem = function(item) {
+ //            console.info('onBeforeUploadItem', item);
+ //        };
+ //        uploader.onProgressItem = function(fileItem, progress) {
+ //            console.info('onProgressItem', fileItem, progress);
+ //        };
+ //        uploader.onProgressAll = function(progress) {
+ //            console.info('onProgressAll', progress);
+ //        };
+ //        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+ //            console.info('onSuccessItem', fileItem, response, status, headers);
+ //        };
+ //        uploader.onErrorItem = function(fileItem, response, status, headers) {
+ //            console.info('onErrorItem', fileItem, response, status, headers);
+ //        };
+ //        uploader.onCancelItem = function(fileItem, response, status, headers) {
+ //            console.info('onCancelItem', fileItem, response, status, headers);
+ //        };
+ //        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+ //            console.info('onCompleteItem', fileItem, response, status, headers);
+ //        };
+ //        uploader.onCompleteAll = function() {
+ //            console.info('onCompleteAll');
+ //        };
+
+ //        console.info('uploader', uploader);
+ //    }])
+.controller('fileUploadController', ['$scope', 'FileUploader', function($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            url: '/property'
+        });
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+    }])
+	.directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
